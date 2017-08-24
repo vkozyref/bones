6e0a86282dea575d4d92830bd7c0e4163ab8a087
Anton Bogatyrov <anton.bogatyrov@captify.co.uk>
2017-08-24 20:58:49 +0300

    change

--- src/page/GameList/spec.js
+++ src/page/GameList/spec.js
@@ -3,7 +3,7 @@ import { shallow } from 'enzyme';
 
 import GameList from './';
 
-test('GameList component', () => {
+test('GameList component 1', () => {
   const wrapper = shallow(<GameList />);
   expect(wrapper).toHaveLength(1);
 });
