e78bdedd20f5a624dc9f5d4eb97753088711a3b9
Anton Bogatyrov <anton.bogatyrov@captify.co.uk>
2017-08-24 20:52:34 +0300

    add simple test

---
+++ src/page/GameList/spec.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+
+import GameList from './';
+
+test('GameList component', () => {
+  const wrapper = shallow(<GameList />);
+  expect(wrapper).toHaveLength(1);
+});
